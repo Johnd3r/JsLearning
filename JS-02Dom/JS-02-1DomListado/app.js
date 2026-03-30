3e2f776384850061afccc8abfd1fb9aa03e6dbe3
Johnd3r <johndernaranjo@gmail.com>
2026-03-30 14:26:30 -0500

    Lista basica completa, se termino el contexto y logica de la clase app.js, e implemento funcionalidad en HTML para mejorar la UX con un boton y formulario donde el usuario dijita sus datos y se actualizan en al lista visible al agregar.

--- JS-02Dom/JS-02-1DomListado/app.js
+++ JS-02Dom/JS-02-1DomListado/app.js
@@ -8,13 +8,31 @@ let listaPersonas = [
 ]
 //creamos una funcion para mostrar nuestros nombres
 function mostrarList(){
-    console.log("Cargando funcion mostrarList()"); //confirmando si inicia la funcion desde la consola
+    /* console.log("Cargando funcion mostrarList()"); */ //confirmando si inicia la funcion desde la consola
     let text = ''; // Creamos una variable vacia que contendra las iteraciones de objetos persona creados de la clase persona.
     
     for (let persona of listaPersonas){
-        console.log(`Nombre: ${persona.nombre} Apellido: ${persona.apellido}`)
-        text += `<li>${persona.nombre} ${persona.apellido}</li>`
+        console.log(`Nombre: ${persona.nombre} Apellido: ${persona.apellido}`) // debug de cada persona
+        text += `<li>${persona.nombre} ${persona.apellido}</li>` // concatena item a la cadena
     }
-    document.getElementById('hola').innerHTML = (text);
-    
+    /* console.log(`${text}`); */
+    document.getElementById('personaLista').innerHTML = (text); // actualiza lista en HTML con la lista ya cargada
+    /* console.log(`Aca esta el error: ${text}`); */
+}
+
+function agregarPersona(){
+    let formuA = document.getElementById('formu'); // capturamos el form por medio de una variable creada 'formuA(A se refiere a auxiliar xd)'
+    let nombreA = formuA['nombreF']; // aca se obtiene el valor del input nombre desde el formulario
+    let apellidoA = formuA['apellidoF']; // aca se obtiene el valor del input apellido desde el formulario
+
+    /* console.log('Debuggeando funcion "agregarPersona"'); */
+
+    if (nombreA.value != '' && apellidoA.value != ''){ // validamos que no se envien campos vacios
+        let persona = new Persona (nombreA.value, apellidoA.value); // crea nueva Persona
+        listaPersonas.push(persona); // agrega al arreglo la persona nueva creada por medio del metodo push (Agrega un nuevo valor a la lista creada previamente hasta lo ultimo de la cola)
+        /* listaPersonas.push(nombreA,apellidoA); */
+        return mostrarList(); // refresca la lista visible
+    } else {
+        alert('Campos vacios no se permiten. Porfavor registrar su nombre completo'); // aviso al usuario
+    }
 }
