52e52f74d45c730fcb8849c4cffdf1c1c3814c25
Johnd3r <johndernaranjo@gmail.com>
2026-04-06 15:37:27 -0500

    --fix  Corregir error de parseo con metodo .toFixed()
    - El metodo toFixed convertia los valores a strings lo cual al momento de sacar porcentajes estaba propenso a generar un bug por obtener valores de string.

--- JS-02Dom/JS-02-3DomPresupuestoApp/presupuestoApp.js
+++ JS-02Dom/JS-02-3DomPresupuestoApp/presupuestoApp.js
@@ -20,24 +20,25 @@ console.log(prueba()); // ¿ue imprime? */
 
 const cargarCabecero = () => {
     console.log('hola :)');
-    document.getElementById('dineroDisponible').innerHTML = `$${calcularDineroDisponible()}`;
-    document.getElementById('totalIngresos').innerHTML = `$${calcularIngresosTotal()}`;
-    document.getElementById('totalEgresos').innerHTML = `$${calcularEgresosTotal()}`;
-    document.getElementById('porcentajeEgreso').innerHTML = `%${calcularPorcentajeEgreso()}`;
-    document.getElementById('porcentajeIngreso').innerHTML = `%${calcularPorcentajeIngreso()}`;
+    document.getElementById('dineroDisponible').innerHTML = `$${calcularDineroDisponible().toFixed(2)}`;
+    document.getElementById('totalIngresos').innerHTML = `$${calcularIngresosTotal().toFixed(2)}`;
+    document.getElementById('totalEgresos').innerHTML = `$${calcularEgresosTotal().toFixed(2)}`;
+    document.getElementById('porcentajeEgreso').innerHTML = `%${calcularPorcentajeEgreso().toFixed(1)}`;
+    document.getElementById('porcentajeIngreso').innerHTML = `%${calcularPorcentajeIngreso().toFixed(1)}`;
+    /* console.log(typeof (580).toFixed(2)); */
 }
 
 const calcularDineroDisponible = () => {
     //se resta el total de ingresos con el total de egresos para sacar el dinero disponible
     let totalDisp = calcularIngresosTotal() - calcularEgresosTotal();
-    /* totalDisp = `$`+totalDisp; */
+    /* totalDisp = totalDisp.toFixed(2); */
     return totalDisp;
 }
 
 const calcularIngresosTotal = () => {
     //Esta funcion va a inciar un contador de dinero en 0 que va ir a ummentando extrayendo los valores de objetos Ingresos totales creados
     let ingresoTotal = 0;
-    for (ingreso of ingresos){ //Lo que estamos haciendo aca es iterar con la variable ingreso cada objeto de la lista
+    for (let ingreso of ingresos){ //Lo que estamos haciendo aca es iterar con la variable ingreso cada objeto de la lista
         ingresoTotal += ingreso.valor; //cuando itera sobre un objeto accede a su valor lo cual se lo suma a ingreso total
     }
     return ingresoTotal; //al completar el recorrido de toda la lista devuelve el valor total final de la suma
@@ -45,7 +46,7 @@ const calcularIngresosTotal = () => {
 
 const calcularEgresosTotal = () => {
     let egresoTotal = 0;
-    for (egreso of egresos){
+    for (let egreso of egresos){
         egresoTotal += egreso.valor;
     }
     return egresoTotal;
@@ -53,12 +54,10 @@ const calcularEgresosTotal = () => {
 
 const calcularPorcentajeEgreso = () =>{
     let porcentajeEgreso = calcularEgresosTotal()/calcularIngresosTotal()*100;
-    porcentajeEgreso = porcentajeEgreso.toFixed(0);
     return porcentajeEgreso;
 }
 
 const calcularPorcentajeIngreso = () =>{
-    let porcentajeIngreso = calcularPorcentajeEgreso()/calcularIngresosTotal()*100;
-    porcentajeIngreso = porcentajeIngreso.toFixed(0);
+    let porcentajeIngreso = calcularDineroDisponible()/calcularIngresosTotal()*100;
     return porcentajeIngreso;
 }
