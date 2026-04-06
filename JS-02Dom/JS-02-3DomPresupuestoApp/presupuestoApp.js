2a10093cb16d25f1453ed78dacdef2494c843f42
Johnd3r <johndernaranjo@gmail.com>
2026-04-06 15:02:27 -0500

    fix-- Formato de valores numericos.
    - Se le dio formato correcto de simobilizacion de dinero a los valores mostrados.
    
    - A su misma vez tambien a los valores de porcentaje, para una mejor UX.

--- JS-02Dom/JS-02-3DomPresupuestoApp/presupuestoApp.js
+++ JS-02Dom/JS-02-3DomPresupuestoApp/presupuestoApp.js
@@ -20,26 +20,27 @@ console.log(prueba()); // ¿ue imprime? */
 
 const cargarCabecero = () => {
     console.log('hola :)');
-    document.getElementById('dineroDisponible').innerHTML = calcularDineroDisponible();
-    document.getElementById('totalIngresos').innerHTML = calcularIngresosTotal();
-    document.getElementById('totalEgresos').innerHTML = calcularEgresosTotal();
-    document.getElementById('porcentajeEgreso').innerHTML = calcularPorcentajeEgreso();
+    document.getElementById('dineroDisponible').innerHTML = `$${calcularDineroDisponible()}`;
+    document.getElementById('totalIngresos').innerHTML = `$${calcularIngresosTotal()}`;
+    document.getElementById('totalEgresos').innerHTML = `$${calcularEgresosTotal()}`;
+    document.getElementById('porcentajeEgreso').innerHTML = `%${calcularPorcentajeEgreso()}`;
+    document.getElementById('porcentajeIngreso').innerHTML = `%${calcularPorcentajeIngreso()}`;
 }
 
 const calcularDineroDisponible = () => {
     //se resta el total de ingresos con el total de egresos para sacar el dinero disponible
     let totalDisp = calcularIngresosTotal() - calcularEgresosTotal();
-    totalDisp = `$`+totalDisp;
+    /* totalDisp = `$`+totalDisp; */
     return totalDisp;
 }
 
 const calcularIngresosTotal = () => {
     //Esta funcion va a inciar un contador de dinero en 0 que va ir a ummentando extrayendo los valores de objetos Ingresos totales creados
     let ingresoTotal = 0;
-    for (ingreso of ingresos){
-        ingresoTotal += ingreso.valor;
+    for (ingreso of ingresos){ //Lo que estamos haciendo aca es iterar con la variable ingreso cada objeto de la lista
+        ingresoTotal += ingreso.valor; //cuando itera sobre un objeto accede a su valor lo cual se lo suma a ingreso total
     }
-    return ingresoTotal;
+    return ingresoTotal; //al completar el recorrido de toda la lista devuelve el valor total final de la suma
 }
 
 const calcularEgresosTotal = () => {
@@ -51,10 +52,13 @@ const calcularEgresosTotal = () => {
 }
 
 const calcularPorcentajeEgreso = () =>{
-    let porcentajeEgreso = calcularIngresosTotal() /calcularEgresosTotal();
+    let porcentajeEgreso = calcularEgresosTotal()/calcularIngresosTotal()*100;
+    porcentajeEgreso = porcentajeEgreso.toFixed(0);
     return porcentajeEgreso;
 }
-/* const registroDatos = () => {
-    let ingresoDatos = [Ingreso(registroDatos)];
-    let EgresoDatos = [Egreso(registroDatos)];
- } */
+
+const calcularPorcentajeIngreso = () =>{
+    let porcentajeIngreso = calcularPorcentajeEgreso()/calcularIngresosTotal()*100;
+    porcentajeIngreso = porcentajeIngreso.toFixed(0);
+    return porcentajeIngreso;
+}
