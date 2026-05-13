c6a06c9db691593b47e45da6f750a215a9a10f3a
Johnder Naranjo <johndersanchez001@gmail.com>
2026-05-12 21:50:45 -0500

    feat: Se completó satisfactoriamente el ejercicio de contar vocales y consonantes de un string!

--- JS-Practice/EasyLevel/contarVocalesyCons.js
+++ JS-Practice/EasyLevel/contarVocalesyCons.js
@@ -2,32 +2,25 @@ const vocales = ["a", "e", "i", "o", "u"];
 
 
 const vocalesString = (palabra, vocales) => {
-
 //objeto contador de vocales
+const contarConsonantes = {}
 const contarVocales = {};
 //creamos un lugar donde guardar en un array las palabras divididas de la palabra a conocer ('Niño')
 let palabraL = palabra.split(""); //lo esperado seria que guardara algo como ["n", "i", "ñ", "o"] 
-
 //Se recorre cada item del array/palabra que se pasa
-    palabraL.forEach((validarVocal) => {
-        //acá se valida que la letra recorrida del array sea igual a la vocal iterada para poder registrarla en el objeto contador de vocales
-        if(validarVocal === vocales[i]){
+    palabraL.forEach((letraIterada) => {
+        //acá se valida que en el array de vocales haya lo mismo que en la letra iterada de la palabra obtenida 'letraIterada', es decir, la 
+        if(vocales.includes(letraIterada)){
         //en caso de ser verdadera se valida si dentro del objeto ya esta la vocal registrada, en caso de que no y arroje "0" , a esa vocal se le suma un registro y se guarda dentro del obj. Y si lo existe anteriormente, se le suma un registro mas y se guarda su registro
-        contarVocales[validarVocal] = (contarVocales[validarVocal] || 0) +1;
-            ++vocales[i];
-            ++validarVocal;
+        contarVocales[letraIterada] = (contarVocales[letraIterada] || 0) +1;
         }        
-        //llegado el caso en el que la condicion de arriba no se cumpla, se itera a la
         else{
-            ++vocales[i];
-            --validarVocal;
+            contarConsonantes[letraIterada] = (contarConsonantes[letraIterada] || 0) +1;
+/*             console.log(`La letra ${letraIterada} no es una vocal.`); */
         }
-});
-
-console.log("Las vocales son:", contarVocales)
-
+    });
+    console.log("Las vocales son:", contarVocales);
+    console.log("Las vocales son:", contarConsonantes);
 }
-
-
 
 vocalesString("niño", vocales);
