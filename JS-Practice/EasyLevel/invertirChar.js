780d34c98963db25f0cf8554a140d0a3b5c92011
Johnder Naranjo <johndersanchez001@gmail.com>
2026-05-26 11:27:29 -0500

    feat: se completo exitosamente ejercicio de validar palabras palindormas.
    - Tiempo aproximado: 19' mn
    - Aprendido: uso del metodo reverse y join
    - Dificultad: facil si se conoce los metodos, no tomaria mas de 5 mm
    - Recursos:
    1. Pista de metodos sin llegar a la respuesta por medio de IA para yo mismo buscar la respuesta.
    
    2. StackOverFlow para buscar metodos similares y diferentes formas de pensar
    
    3.mdn (DeveloperMozilla.org) para leer documentacion tecnica de los metodos usados.

---
+++ JS-Practice/EasyLevel/invertirChar.js
@@ -0,0 +1,23 @@
+
+const validarPalindroma = (palabra) => {
+
+let palabraAlRevez = palabra.split("").reverse().join(""); // usamos el metodo de arraydear la palabra pasada como parametro con 'split', luego la reverseamos con el metodo reverse --> esrever, despues concatenamos todo con el metodo join y le pasamos como parametro "," para eliminar los , que hay entre las palabras y formar un solo string
+
+if (palabraAlRevez === palabra){  //validamos si la palabra es tal cual del mismo tipo y valor y asi confirmar si es palindroma
+    console.log(`La palabra ${palabra} es palindroma, se escribe igual al derecho y al revez`);
+    console.log("palabra normal: ", palabra);
+    console.log("palabra alrevez: ", palabraAlRevez);
+    console.log("----------------------------------------------------------------------------------")    
+} else{ // si no es palindroma se arroja el porque y la diferencia de las palabras 
+        console.log(`La palabra ${palabra} no es palindroma, se escribe diferente al derecho y al revez: ¡${palabra} <>  ${palabraAlRevez}!`);
+        console.log("palabra normal: ", palabra);
+        console.log("palabra alrevez: ", palabraAlRevez);
+        console.log("----------------------------------------------------------------------------------")
+    }
+}
+
+//Valores random para testear
+validarPalindroma('radar'); //palindroma radar === radar
+validarPalindroma('amor'); //no palindroma amor !== roma
+validarPalindroma('reconocer'); //palindroma reconocer === reonocer
+validarPalindroma('arroz'); // no palindroma arroz !== zorra
